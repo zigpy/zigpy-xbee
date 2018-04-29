9ebb47bc66023c954002a14f572cf570e66a6679
Russell Cloran <rcloran@gmail.com>
2018-04-29 13:04:14 -0700

    Use new zigpy application deserialize

--- tests/test_application.py
+++ tests/test_application.py
@@ -1,3 +1,5 @@
+from unittest import mock
+
 import pytest
 
 from zigpy_xbee.api import XBee
@@ -15,3 +17,68 @@ def test_modem_status(app):
     # assert that the test below actually checks a value that is not in MODEM_STATUS
     assert 0xff not in app._api.MODEM_STATUS
     app.handle_modem_status(0xff)
+
+
+def _test_rx(app, device, deserialized):
+    app.get_device = mock.MagicMock(return_value=device)
+    app.deserialize = mock.MagicMock(return_value=deserialized)
+
+    app.handle_rx(
+        b'\x01\x02\x03\x04\x05\x06\x07\x08',
+        mock.sentinel.src_nwk,
+        mock.sentinel.src_ep,
+        mock.sentinel.dst_ep,
+        mock.sentinel.cluster_id,
+        mock.sentinel.profile_id,
+        mock.sentinel.rxopts,
+        b'',
+    )
+
+    assert app.deserialize.call_count == 1
+
+
+def test_rx(app):
+    device = mock.MagicMock()
+    app.handle_message = mock.MagicMock()
+    _test_rx(app, device, (1, 2, False, []))
+    assert app.handle_message.call_count == 1
+    assert app.handle_message.call_args == ((
+        device,
+        False,
+        mock.sentinel.profile_id,
+        mock.sentinel.cluster_id,
+        mock.sentinel.src_ep,
+        mock.sentinel.dst_ep,
+        1,
+        2,
+        [],
+    ), )
+
+
+def test_rx_reply(app):
+    app._handle_reply = mock.MagicMock()
+    _test_rx(app, mock.MagicMock(), (1, 2, True, []))
+    assert app._handle_reply.call_count == 1
+
+
+def test_rx_failed_deserialize(app, caplog):
+    app._handle_reply = mock.MagicMock()
+    app.handle_message = mock.MagicMock()
+    app.get_device = mock.MagicMock(return_value=mock.sentinel.device)
+    app.deserialize = mock.MagicMock(side_effect=ValueError)
+
+    app.handle_rx(
+        mock.sentinel.src_ieee,
+        mock.sentinel.src_nwk,
+        mock.sentinel.src_ep,
+        mock.sentinel.dst_ep,
+        mock.sentinel.cluster_id,
+        mock.sentinel.profile_id,
+        mock.sentinel.rxopts,
+        b'',
+    )
+
+    assert any(record.levelname == 'ERROR' for record in caplog.records)
+
+    assert app._handle_reply.call_count == 0
+    assert app.handle_message.call_count == 0
