e8bb1347278bc5a542fa2d2980932f1028c3ef39
Alexei Chetroi <alexei.chetroi@outlook.com>
2019-03-31 17:58:15 -0400

    Bump version to 0.1.4.dev0

---
+++ zigpy_xbee/const.py
@@ -0,0 +1,6 @@
+"""Constants used by zigpy-xbee."""
+MAJOR_VERSION = 0
+MINOR_VERSION = 1
+PATCH_VERSION = "4.dev0"
+__short_version__ = '{}.{}'.format(MAJOR_VERSION, MINOR_VERSION)
+__version__ = '{}.{}'.format(__short_version__, PATCH_VERSION)
