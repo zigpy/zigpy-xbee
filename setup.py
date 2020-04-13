485c5f9f33bf37714673c7797dc4e55939bc2644
Alexei Chetroi <alexei.chetroi@outlook.com>
2020-04-13 17:49:12 -0400

    Bump up zigpy dependency.

--- setup.py
+++ setup.py
@@ -21,6 +21,6 @@ setup(
     author_email="rcloran@gmail.com",
     license="GPL-3.0",
     packages=find_packages(exclude=["*.tests"]),
-    install_requires=["pyserial-asyncio", "zigpy-homeassistant >= 0.17.0"],
+    install_requires=["pyserial-asyncio", "zigpy>= 0.20.a1"],
     tests_require=["pytest"],
 )
