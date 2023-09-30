f85233fc28ae01c08267965e99a29e43b00e1561
Denis Shulyaka <Shulyaka@gmail.com>
2023-09-30 23:30:19 +0300

    Fix ci (#154)
    * Migrate to pyproject.toml and Git versioning
    
    * Use shared PyPi release workflow
    
    * Remove release drafter config
    
    * add ci.yml
    
    * add requirements_test.txt
    
    * add matchers
    
    * fix py3.12 tests
    
    * fix ci.yml
    
    * increase minimum coverage
    
    * Update PYTHON_VERSION_DEFAULT

--- setup.py
+++ setup.py
@@ -1,22 +1,6 @@
-"""Setup module for zigpy-xbee"""
+"""Setup stub for legacy builds."""
 
-import pathlib
+import setuptools
 
-from setuptools import find_packages, setup
-
-import zigpy_xbee
-
-setup(
-    name="zigpy-xbee",
-    version=zigpy_xbee.__version__,
-    description="A library which communicates with XBee radios for zigpy",
-    long_description=(pathlib.Path(__file__).parent / "README.md").read_text(),
-    long_description_content_type="text/markdown",
-    url="http://github.com/zigpy/zigpy-xbee",
-    author="Russell Cloran",
-    author_email="rcloran@gmail.com",
-    license="GPL-3.0",
-    packages=find_packages(exclude=["tests", "tests.*"]),
-    install_requires=["zigpy>=0.56.0"],
-    tests_require=["pytest", "asynctest", "pytest-asyncio"],
-)
+if __name__ == "__main__":
+    setuptools.setup()
